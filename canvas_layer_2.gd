93f73ddc8e2ba97d4d27a2a849444e69fa312dc1
Yggdrasil938 <queuettree@gmail.com>
2024-10-04 08:14:28 -0500

    demo 1 release. Adding enemies, enemy spawning and bullet to enemy collisions

---
+++ canvas_layer_2.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+var c_enemy = preload("res://red chip.tscn")
+var position: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if get_node("Enemy Spawn Timer").is_stopped():
+		var enemy_instance = c_enemy.instantiate()
+		print("created enemy")
+		get_tree().root.add_child(enemy_instance)
+		randomize()
+		var x_range = Vector2(100, 750)
+		var y_range = Vector2(100, 750)
+
+		var random_x = randi() % int(x_range[1]- x_range[0]) + 1 + x_range[0] 
+		var random_y =  randi() % int(y_range[1]-y_range[0]) + 1 + y_range[0]
+		var random_pos = Vector2(random_x, random_y)
+
+		position=random_pos
+		enemy_instance.global_position = position
+		get_node("Enemy Spawn Timer").start()
+	pass
--- red_chip.gd
+++ red_chip.gd
@@ -1,23 +1,21 @@
 extends Area2D
-@export var move_speed = 2000
-var direction = Vector2 (1,0)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("creating bullet")
-	await get_tree().create_timer(3).timeout
-	queue_free()
-	print("bullet destroyed")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position += direction * move_speed * delta
 	pass
+	
+
+
+
 
 func _on_area_entered(body: Area2D) -> void:
-	if body.is_in_group("enemies"):
+	if body.is_in_group("bullets"):
+		print("enemy hit!")
 		queue_free()
 	pass # Replace with function body.
